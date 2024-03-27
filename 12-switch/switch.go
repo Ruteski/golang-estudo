a1a8dd45473df37806921cf6fdc88d843dc6f534
Lincoln Ruteski <lruteski@gmail.com>
2024-03-27 14:44:02 -0300

    12-switch

---
+++ 12-switch/switch.go
@@ -0,0 +1,71 @@
+package main
+
+import "fmt"
+
+func diaDaSemana(numero int) string {
+	switch numero {
+	case 1:
+		return "Domingo"
+
+	case 2:
+		return "Segunda"
+
+	case 3:
+		return "Terça"
+
+	case 4:
+		return "Quarta"
+
+	case 5:
+		return "Quinta"
+
+	case 6:
+		return "Sexta"
+
+	case 7:
+		return "Sabado"
+
+	default:
+		return "Numero invalido"
+	}
+}
+
+func diaDaSemana2(numero int) string {
+	var diaDaSemana string
+
+	switch {
+	case numero == 1:
+		diaDaSemana = "Domingo"
+
+	case numero == 2:
+		diaDaSemana = "Segunda"
+
+	case numero == 3:
+		diaDaSemana = "Terça"
+
+	case numero == 4:
+		diaDaSemana = "Quarta"
+
+	case numero == 5:
+		diaDaSemana = "Quinta"
+
+	case numero == 6:
+		diaDaSemana = "Sexta"
+
+	case numero == 7:
+		diaDaSemana = "Sabado"
+
+	default:
+		diaDaSemana = "Numero invalido"
+	}
+
+	return diaDaSemana
+}
+
+func main() {
+	dia := diaDaSemana(3)
+	fmt.Println(dia)
+
+	dia2 := diaDaSemana2(50)
+	fmt.Println(dia2)
+}
