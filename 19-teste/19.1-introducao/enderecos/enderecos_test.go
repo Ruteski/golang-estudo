0338df50b4e6282cd4d5218163d5592c4c9e839e
Lincoln Ruteski <lruteski@gmail.com>
2024-04-04 14:59:40 -0300

    19 - refatorado o teste unitario para abranger diversos cenarios

--- 19-teste/19.1-introducao/enderecos/enderecos_test.go
+++ 19-teste/19.1-introducao/enderecos/enderecos_test.go
@@ -4,12 +4,27 @@ package enderecos
 
 import "testing"
 
-func TestTipoEndereco(t *testing.T) {
-	enderecoTeste := "Avenida Paulista"
-	tipoEnderecoEsperado := "Avenida"
-	tipoEnderecoRecebido := TipoEndereco(enderecoTeste)
+type cenarioTeste struct {
+	enderecoInserido string
+	retornoEsperado  string
+}
 
-	if tipoEnderecoRecebido != tipoEnderecoEsperado {
-		t.Errorf("O tipo recebido é diferente do esperado! Esperava %s e recebeu %s", tipoEnderecoEsperado, tipoEnderecoRecebido)
+func TestTipoEndereco(t *testing.T) {
+	cenariosTeste := []cenarioTeste{
+		{"Rua ABC", "Rua"},
+		{"Avenida Paulista", "Avenida"},
+		{"Rodovia dos Imigrantes", "Rodovia"},
+		{"Praça dos Polacos", "Tipo inválido"},
+		{"Estrada das Ortigueiras", "Estrada"},
+		{"RUA DAS RUAS", "Rua"},
+		{"rua do chisto", "Rua"},
+		{"", "Tipo inválido"},
+	}
+
+	for _, cenario := range cenariosTeste {
+		tipoEnderecoRecebido := TipoEndereco(cenario.enderecoInserido)
+		if tipoEnderecoRecebido != cenario.retornoEsperado {
+			t.Errorf("O tipo recebido é diferente do esperado! Esperava '%s' e recebeu '%s'", cenario.retornoEsperado, tipoEnderecoRecebido)
+		}
 	}
 }
